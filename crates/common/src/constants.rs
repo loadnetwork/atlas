26268c72e367a97d2f32dfba683d3f395abd15d6
Rani Elhusseini <charmful0x@gmail.com>
2025-11-26 10:47:08 +0100

    feat: common's gateway stack

---
+++ crates/common/src/constants.rs
@@ -0,0 +1,9 @@
+// FLP system yield oracle processes
+pub const USDS_ORACLE_PID: &str = "qjOMZnan8Vo2gaLaOF1FXbFXOQOn_5sKbYspNSVRyNY";
+pub const STETH_ORACLE_PID: &str = "wJV8FMkpoeLsTjJ6O7YZEuQgMqj-sDjPHhTeA73RsCc";
+pub const DAI_ORACLE_PID: &str = "5q8vpzC5QAKOAJFM26MAKfZw1gwtw7WA_J2861ZiKhI";
+// ao protocol authority address
+pub const AO_AUTHORITY: &str = "fcoN_xJeisVsPXA-trzVAuIiqO3ydLQxM-L4XbrQKzY";
+
+// endpoints
+pub const ARWEAVE_GATEWAY: &str = "https://arweave.net";
---
+++ crates/common/src/gateway.rs
@@ -0,0 +1,9 @@
+use anyhow::Error;
+use crate::constants::ARWEAVE_GATEWAY;
+
+/// downloads an Arweave `txid` data and return Vec<u8> Body
+pub fn download_tx_data(txid: &str) -> Result<Vec<u8>, Error> {
+    let url = format!("{ARWEAVE_GATEWAY}/{txid}");
+    let mut req = ureq::get(url).call()?;
+    Ok(req.body_mut().read_to_vec()?)
+}
